6aba2556d97929579b7f31cf0927d001a56658a9
Ilya Uvarenkov <trukanduk@gmail.com>
2016-10-22 22:44:46 +0300

    map stub

--- html/js/Map.js
+++ html/js/Map.js
@@ -1,1 +1,26 @@
-// TODO
+$(document).ready(function() {
+    var mapElement = $("#map-wrap");
+    var map = L.map('map', {
+        center: [55.755, 37.6],
+        zoom: 11
+    });
+
+    L.tileLayer(
+        "http://c.tile.osm.kosmosnimki.ru/night/{z}/{x}/{y}.png", {
+        maxZoom: 14,
+        attribution: "Map data &copy <a href='http://openstreetmap.org' alt='OpenStreetMap'>OpenStreetMap</a> , tiles &copy <a href='http://osm.kosmosnimki.ru' alt='kosmosnimri'>Kosmosnimki</a>",
+    }).addTo(map);
+
+    for (var aoname in kAoCoords) {
+        var polygon = L.polygon(kAoCoords[aoname], {
+            fillColor: 'white',
+            fillOpacity: 0.0,
+            color: 'white',
+            weight: 1,
+        }).on('mouseover', function() {
+            this.setStyle({fillOpacity: 0.1, weight: 2});
+        }).on('mouseout', function() {
+            this.setStyle({fillOpacity: 0.0, weight: 1});
+        }).addTo(map);
+    }
+});
---
+++ tools/ao_coords.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+
+import argparse
+import re
+import json
+
+def parsePolygon(ss):
+	data = []
+	for item in map(lambda item: item.split(' '), ss.split(',')):
+		if len(item) == 2:
+			data.append([float(item[1]), float(item[0])])
+	return data
+
+def main(args):
+	result = {}
+	for index, line in enumerate(args.inp):
+		if (index == 0 and not args.noskipheader) or not line.strip():
+			continue
+
+		shapeEnd = line.find("\"", 2)
+		shape = line[1:shapeEnd]
+		rest = line[shapeEnd+1:]
+		if shape[:7] == "POLYGON":
+			data = parsePolygon(shape[10:-2])
+			# continue
+		elif shape[:12] == "MULTIPOLYGON":
+			# continue
+			data = []
+			polyre = re.compile(r"\(\((?P<data>(?:\d+\.\d+ \d+\.\d+,)*\d+\.\d+ \d+\.\d+)\)\)")
+			for polygon in polyre.finditer(shape[14:-1]):
+
+				data.append(parsePolygon(polygon.group("data")))
+
+		name = rest.split(",")[1]
+
+		result[name] = data
+
+	print("var {} = ".format(args.varname), file=args.outp, end="")
+	json.dump(result, args.outp)
+
+if __name__ == "__main__":
+	parser = argparse.ArgumentParser("Tool for translating AO coords to json.")
+	parser.add_argument("inp", type=argparse.FileType("r", encoding="utf8"), help="input file (*csv)")
+	parser.add_argument("outp", type=argparse.FileType("w", encoding="utf8"), help="output file (*.json|*.js)")
+	parser.add_argument("-varname", default="kAoCoords", help="Variable name")
+	parser.add_argument("-noskipheader", action="store_false", default="true", help="Skip first line (headers)")
+	args = parser.parse_args()
+
+	main(args)
